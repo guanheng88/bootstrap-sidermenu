e1644a057b5ef942d0ae8b8744a308bbd74a3e27
管恒 <guanheng@eastmoney.com>
2019-06-24 16:35:42 +0800

    replace tab to space

--- js/bootstrap-sidermenu.js
+++ js/bootstrap-sidermenu.js
@@ -3,24 +3,24 @@
  * version: 1.0.0
  */
 $(function() {
-	var menu = $(".menu-siderbar");
-	var menuItem = menu.find(".nav-item");
+    var menu = $(".menu-siderbar");
+    var menuItem = menu.find(".nav-item");
 
-	menuItem.find(".nav-link-parent").click(function() {
-		var parentItem = $(this).parent();
-		var isOpen = parentItem.hasClass("open");
+    menuItem.find(".nav-link-parent").click(function() {
+        var parentItem = $(this).parent();
+        var isOpen = parentItem.hasClass("open");
 
-		menuItem.each(function(i, element) {
-			if ($(this).hasClass("open")) {
-				$(this).removeClass("open");
-			}
-		});
+        menuItem.each(function(i, element) {
+            if ($(this).hasClass("open")) {
+                $(this).removeClass("open");
+            }
+        });
 
-		if (isOpen) {
-			parentItem.removeClass("open");
-		}
-		else {
-			parentItem.addClass("open");
-		}
-	});
+        if (isOpen) {
+            parentItem.removeClass("open");
+        }
+        else {
+            parentItem.addClass("open");
+        }
+    });
 });
